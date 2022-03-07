2a0fd5a917e25afe488cd01899bb52f83f36e127
111011896 <101096608+111011896@users.noreply.github.com>
2022-03-07 18:37:11 +0900

    Create Complementing a Strand of DNA.py

---
+++ Complementing a Strand of DNA.py
@@ -0,0 +1,15 @@
+with open("새파일.txt") as f:
+    text = f.read()
+
+blink = ""
+for i in range(len(text)):
+    if text[i] == "A":
+        blink += "T"
+    elif text[i] == "C":
+        blink += "G"
+    elif text[i] == "T":
+        blink += "A"
+    elif text[i] == "G":
+        blink += "C"
+
+print(blink[::-1])
