55295142de987293561286e041fa1238aca60f37
111011896 <101096608+111011896@users.noreply.github.com>
2022-03-09 10:02:35 +0900

    Create Merge Sort.py

---
+++ Merge Sort.py
@@ -0,0 +1,48 @@
+with open('새파일.txt') as file:
+    lines = []
+    for line in file:
+        lines.append(line.split())
+
+unsorted_list = []
+
+for i in lines[1]:
+    unsorted_list.append(int(i))
+
+def merge(left, right):
+    i = 0
+    j = 0
+    sorted_list = []
+
+    while (i < len(left)) & (j < len(right)):
+        if left[i] < right[j]:
+            sorted_list.append(left[i])
+            i += 1
+        else:
+            sorted_list.append(right[j])
+            j += 1
+
+    while (i < len(left)):
+        sorted_list.append(left[i])
+        i += 1
+
+    while (j < len(right)):
+        sorted_list.append(right[j])
+        j += 1
+
+    return sorted_list
+
+def merge_sort(unsorted_list):
+
+    if len(unsorted_list) <= 1:
+        return unsorted_list
+
+    mid = len(unsorted_list) // 2
+    left = unsorted_list[:mid]
+    right = unsorted_list[mid:]
+
+    left1 = merge_sort(left)
+    right1 = merge_sort(right)
+    return merge(left1, right1)
+
+for i in merge_sort(unsorted_list):
+    print(i, end=' ')
