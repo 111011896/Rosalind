7e479e8181e08e10afc98e5a6ce1b25f02f69a22
111011896 <101096608+111011896@users.noreply.github.com>
2022-03-10 00:23:48 +0900

    Update Merge Sort.py

--- Merge Sort.py
+++ Merge Sort.py
@@ -12,7 +12,6 @@ def merge(left, right):
     i = 0
     j = 0
     sorted_list = []
-
     while (i < len(left)) & (j < len(right)):
         if left[i] < right[j]:
             sorted_list.append(left[i])
@@ -20,15 +19,12 @@ def merge(left, right):
         else:
             sorted_list.append(right[j])
             j += 1
-
     while (i < len(left)):
         sorted_list.append(left[i])
         i += 1
-
     while (j < len(right)):
         sorted_list.append(right[j])
         j += 1
-
     return sorted_list
 
 def merge_sort(unsorted_list):
