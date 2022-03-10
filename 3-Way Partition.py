90803980dc75005c6a582ec9b32df17a3b3af955
111011896 <101096608+111011896@users.noreply.github.com>
2022-03-10 21:32:33 +0900

    Create 3-Way Partition.py

---
+++ 3-Way Partition.py
@@ -0,0 +1,25 @@
+with open('새파일.txt') as file:
+    lines = []
+    for line in file:
+        lines.append(line.split())
+
+arr = []
+
+for i in lines[1]:
+    arr.append(int(i))
+
+pivot = arr[0]
+lesser_arr, equal_arr, greater_arr = [], [], []
+
+for i in arr:
+    if i < pivot:
+        lesser_arr.append(i)
+    elif i > pivot:
+        greater_arr.append(i)
+    else:
+        equal_arr.append(i)
+
+total_arr = lesser_arr + equal_arr + greater_arr
+
+for i in total_arr:
+    print(i, end=' ')
